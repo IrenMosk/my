ef8e5ccf7a7618dd77ebe96481c8ead20e5d85dc
IrenMosk <IrinaMoskviceva854@gmail.com>
2024-12-20 21:22:07 +0300

    finish two section

--- scripts/observer.js
+++ scripts/observer.js
@@ -1,20 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const observer = new IntersectionObserver(
-    (entries) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          entry.target.setAttribute("data-in-view", "true");
-        } else {
-          entry.target.setAttribute("data-in-view", "false");
-        }
-      });
-    },
-    {
-      root: null,
-      threshold: 0.3,
-    }
-  );
-
   const sections = document.querySelectorAll(".track-section");
-  sections.forEach((section) => observer.observe(section));
+
+  sections.forEach((section) => {
+    // Получаем значение threshold из атрибута data-threshold или задаем значение по умолчанию
+    const thresholdValue =
+      parseFloat(section.getAttribute("data-threshold")) || 0.3;
+
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            entry.target.setAttribute("data-in-view", "true");
+          } else {
+            entry.target.setAttribute("data-in-view", "false");
+          }
+        });
+      },
+      {
+        root: null,
+        threshold: thresholdValue,
+      }
+    );
+
+    observer.observe(section);
+  });
 });
